cd3be4f2d4b66dc14d7ccff9286cd3b1289db038
Marcell Kujbus <marcellkujbus@gmail.com>
2020-04-27 20:03:21 +0200

    diffeqs implemented

--- R/empirical.R
+++ R/empirical.R
@@ -10,10 +10,13 @@ LazyEval_cheatcode_dfe <- function(m,n) {
 
 kmn_seir_equations <- function(time, variables, parameters) {
   with(as.list(c(variables, parameters)), {
-    print(tau_q)
-    print(res["t"])
-    dS <- -(k*b*I+q*k*(1-b)*I_S)*S/N + (q*k*(1-b)*lagvalue(tau_q, S)*lagvalue(tau_q, I_s))/N 
-    dS_Q <- (q*k*(1-b)*S*I_S)/N - (q*k*(1-b)*lagvalue(tau_q, S)*lagvalue(tau_q, I_S))/N
+    if (time <= tau_q+1){
+      lagged = rep(0, 2)} #this is for S(t-tau_q) I_S(t-tau_q)
+    else {
+      lagged = lagvalue(time - tau_q)[c(1, 11)]
+    }
+    dS <- -(k*b*I+q*k*(1-b)*I_S)*S/N + (q*k*(1-b) * lagged[1] * lagged[2] )/N 
+    dS_Q <- (q*k*(1-b)*S*I_S)/N - (q*k*(1-b) * lagged[1] * lagged[2] )/N
     dE1 <- (k*b*(I - q*I_S))*S/N - m*sigma*E1
     dE2 <- m * sigma * E1 - m * sigma * E2
     #for (i in 2:m) {
@@ -53,30 +56,28 @@ kmn_seir_equations <- function(time, variables, parameters) {
     
     dQ <- (q*k*b*S*I_S)/N + d_I*I_S
     
-    dR <- n*gamma*I_A + n*gamma*(1-mu)*I_S
+    dR <- n*gamma*(I_A3 + I_S3)
     
-    dD <- n*gamma*mu*I_S
-    
+
     dP_I1 = 0
     dP_I2 = 0
     dP_I3 = 0
 
     
-    return(list(c(dS, dS_Q, dE1, dE2, dI_A1,dI_A2,dI_A3, dI_S1,dI_S2,dI_S3, dI_S, dI_A, dI, dP_I1, dP_I2, dP_I3, dQ, dR, dD)))
+    return(list(c(dS, dS_Q, dE1, dE2, dI_A1,dI_A2,dI_A3, dI_S1,dI_S2,dI_S3, dI_S, dI_A, dI, dP_I1, dP_I2, dP_I3, dQ, dR)))
   })
 }
 
 parameter_values <- c(
   k=100,
-  b=0.04,
-  q=0.1,
-  N=500,
+  b=0.004,
+  q=1,
+  N=504,
   m=2,
-  sigma=0.5,
+  sigma=0.1,
   n=3,
   gamma=1/14,
-  mu=0.1,
-  d_I=0.2,
+  d_I=0.8,
   tau_q = 14
 )
 
@@ -87,26 +88,29 @@ vector_maker <- function(str, MorN) {
   return(alls)
 }
 
+p_gen <- function(m, sigma, E_m_lagged, tau_d, n, gamma, i) {
+  return(m * sigma * E_m_lagged * exp(-n*gamma*tau_d)*((n*gamma*tau_d)^(i-1))/(i-1))
+}
+
 initial_values <- c(
   S=500,
   S_Q=0,
-  E1 = 50,
-  E2 = 100,
-  I_A1 = 43,
-  I_A2 = 32,
-  I_A3 = 14,
-  I_S1 = 2,
-  I_S2 = 1,
+  E1 = 1,
+  E2 = 1,
+  I_A1 = 1,
+  I_A2 = 0,
+  I_A3 = 0,
+  I_S1 = 1,
+  I_S2 = 0,
   I_S3 = 0,
-  I_S = 3,
-  I_A = 89,
-  I=92,
-  P_I1 = 0.1,
-  P_I2 = 0.1,
-  P_I3 = 0.1,
+  I_S = 1,
+  I_A =1,
+  I=2,
+  P_I1 = 0.0,
+  P_I2 = 0.0,
+  P_I3 = 0.0,
   Q=0,
-  R=0,
-  D=0
+  R=0
 )
 
 time_values <- seq(0:100)
@@ -126,8 +130,36 @@ with(res, {
   lines(time, I, col = "red")
   # adding the time series of recovered:
   lines(time, R, col = "green")
+  lines(time, Q, col="black")
+  lines(time, S_Q, col="purple")
 })
 
 # adding a legend:
-legend("right", c("susceptibles", "infectious", "recovered"),
-       col = c("blue", "red", "green"), lty = 1, bty = "n")
+legend("right", c("susceptibles", "infectious", "recovered", "quarantine"),
+       col = c("blue", "red", "green", "black"), lty = 1, bty = "n")
+
+write.csv(res, "C:/OwnWork/thesis/results.csv")
+
+
+kmn_seir <- function(parameter_values, initial_values, time_values) {
+  res = dede(
+    y=initial_values,
+    times=time_values,
+    func=kmn_seir_equations,
+    parms = parameter_values
+  )
+  return(as.data.frame(res))
+}
+
+flu <- read.table("https://bit.ly/2vDqAYN", header = TRUE)
+flu
+preds <- kmn_seir(parameter_values, initial_values, flu$day)
+
+
+
+
+
+
+
+
+
