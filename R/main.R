18bccaf1108b4729c67f4bcc1b92d4ea1973eca0
Marcell Kujbus <marcellkujbus@gmail.com>
2020-04-30 10:02:47 +0200

    changed main

--- R/main.R
+++ R/main.R
@@ -1,10 +1,12 @@
 setwd("C:/Marci/CEU/ThesisDONTUSETHIS/CODE/R")
-source("MN-SEIR-MODELS/m3n5.R")
-flu = read.csv("flu.csv")
-cases = flu$cases
+
+path <- function(m,n) {
+  return(paste0("MN-SEIR-MODELS/m", m, "n", n, ".R"))
+}
+
 
 parameter_values <- c(
-  k=100,
+  k=350,
   b=0.05,
   q=0.02,
   sigma=1,
@@ -12,18 +14,23 @@ parameter_values <- c(
   d_I=0.5,
   tau_d = 2
 )
-
-
-initial_params = parameter_values
-cl <- makeCluster(detectCores())
-start = proc.time()
-optimum <- optimParallel(par=initial_params,
-                         fn=mn_optim,
-                         lower=rep(0, 7),
-                         control=list(maxit=150, trace=6, fnscale=1),
-                         parallel=list(cl=cl))
-end = proc.time()
-stopCluster(cl)
-
-end - start
+res <- data.frame(matrix(rep(0, 25), nrow=5))
+for (m in 1:5) {
+  for (n in 1:5) {
+    source(path(m,n))
+    initial_params = parameter_values
+    cl <- makeCluster(detectCores())
+    optimum <- optimParallel(par=initial_params,
+                             fn=mn_optim,
+                             lower=rep(0, 7),
+                             upper = c(700, 1, 1, Inf, Inf, 1, Inf),
+                             control=list(maxit=150, trace=T, fnscale=1),
+                             parallel=list(cl=cl))
+    res[m, n] = optimum$value
+    print(paste0("m is ",m,", n is ",n))
+    print(paste("rmse is ", optimum$value))
+    stopCluster(cl)
+    
+  }
+}
 
