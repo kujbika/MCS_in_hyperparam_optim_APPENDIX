6ed614348262f1d6c48161b11db0f6e828319473
Marcell Kujbus <marcellkujbus@gmail.com>
2020-05-10 17:23:45 +0200

    change ar results

--- R/MCS_Autoregression/mle_ar.R
+++ R/MCS_Autoregression/mle_ar.R
@@ -3,9 +3,10 @@ library(MASS)
 library (MCS)
 library(parallel)
 library(data.table)
-load("MLE_AR.RData")
 
-
+#parameter declarations for the AR process
+#alpha is the intercept, phi is the coeff vector. Phi can be any long
+#sigma2 is the variance
 alpha <- 5
 phi <-  c(0.8, .25, -0.4, 0.1) # phi_1, phi_2, phi_3. order matters!
 isStationer <- function(phi) {
@@ -17,10 +18,10 @@ isStationer <- function(phi) {
 }
 phi = isStationer(phi)
 sigma2 <- 1.2
-y = arima.sim(n = 1000, list(ar=c(phi)), sd=sqrt(sigma2), mean=alpha)
-
 
 estimator <- function(y, modelling_params) {
+  ###parameter estimation function based on Conditional maximum likelihoog maximization
+  ###for details see https://github.com/kujbika/MCS_in_hyperparam_optim_APPENDIX/tree/master/R/MCS_Autoregression
   row_number = length(y) - modelling_params + 1
   vec = matrix(rev(y)[1 : modelling_params + 1], ncol = modelling_params)
   for (i in 3:row_number) {
@@ -33,10 +34,9 @@ estimator <- function(y, modelling_params) {
   sigma2_est =  (1/row_number) * t(head(rev(y), row_number-1) - X %*% phi) %*% (head(rev(y), row_number-1) - X %*% phi)
   return (list(phi, sigma2_est))
 }
-estimator(y, 4) 
 #-------------------------------------------------------------
 pred.n.ahead <- function(y, n.ahead, parms) {
-  
+  ### n ahead prediction based on given parameters
   if (n.ahead == 0) {
     return(y)
   }else {
@@ -47,8 +47,9 @@ pred.n.ahead <- function(y, n.ahead, parms) {
 }
 
 resid_generator <- function(y, i_vec, n.ahead) {
-  # y is the time series that follows an AR(p)
-  # i_vec is a vector with the possible orders, e.g 1:8 if the real order is p=4
+  ### y is the time series that follows an AR(p)
+  ### i_vec is a vector with the possible orders, e.g 1:8 if the real order is p=4
+  ### returns n.ahead prediction residual terms
   pars <- vector(mode="list", length=length(i_vec))
   names(pars) = i_vec
   coeffs <- vector(mode="list", length=length(i_vec))
@@ -59,58 +60,60 @@ resid_generator <- function(y, i_vec, n.ahead) {
     forecast = pred.n.ahead(u, n.ahead, parms[[1]])
     coeffs[[k]] = parms
     pars[[k]] = (tail(forecast, n.ahead) - tail(y, n.ahead))^2
-  }
+    }
   return (list(as.data.frame(pars), coeffs)) }
 
+#research
+n.ahead=20
 y = arima.sim(n = 1000, list(ar=c(phi)), sd=sqrt(sigma2), mean=alpha)
-df = resid_generator(y, 1:8, 16)
+df = resid_generator(y, 1:8, n.ahead)
+d=apply(df[[1]], 2, mean)
 cl=makeCluster(detectCores())
-MCSprocedure(df[[1]], alpha=0.05, cl=cl)
+MCSprocedure(df[[1]], alpha=0.1, cl=cl)
 stopCluster(cl)
-df2 <- melt(cbind(1:16, df[[1]]),  id.vars = '1:16', variable.name = 'series')
-ggplot(df2, aes(`1:16`,value)) + geom_line(aes(colour = series))
+df2 <- melt(cbind(1:n.ahead, df[[1]]),  id.vars = "1:n.ahead", variable.name = 'series')
+ggplot(df2, aes(`1:n.ahead`,value)) + geom_line(aes(colour = series))
 
-
+#--------------------------------------
 grid_search <- function(y, n.ahead, i_vec) {
+  ### choses the parameter that has produced the minimal loss
+  ### returns the best coefficients, and all the others as well
   df_list = resid_generator(y, i_vec, n.ahead)
   best = which.min(apply(df_list[[1]], 2, sum))
-  return(df_list[[2]][[best]])
+  return(list(df_list[[2]][[best]], df_list[[2]]))
 }
 
-grid_search_forecast_error <- function(y, n.ahead, test.period, i_vec) {
+forecast_errors <- function(y, n.ahead, test.period, i_vec) {
+  ### final comparison based on grid search and MCS predictions
   u = head(y, length(y) - test.period)
   parms = grid_search(u, n.ahead, i_vec)
-  forecast = pred.n.ahead(head(u, length(u) - n.ahead), n.ahead+test.period, parms[[1]])
-  return((tail(forecast, test.period) - tail(y, test.period))^2)
-  
-}
-
-
-loss_values <- function(alpha, phi, sigma2) {
-  # alpha is the intercept of the AR(p)
-  # phi is the real coeff. vector of the same AR(p)
-  # this is only run for experimantla purposes, so i_vec here is alwayzs 1:8
-  
-  loss = data.frame("loss_1" = NA,
-                    "loss_2" = NA,
-                    "loss_3" = NA,
-                    "loss_4" = NA,
-                    "loss_5" = NA,
-                    "loss_6" = NA,
-                    "loss_7" = NA,
-                    "loss_8" = NA)
-  for (k in 1:500) 
-    {
-    y = arima.sim(n = 500, list(ar=c(phi)), sd=sqrt(sigma2), mean=alpha)
-    resids = resid_generator(y, 1:8)
-    loss = rbind(loss, c(resids))
+  forecast_grid = pred.n.ahead(u, test.period, parms[[1]][[1]])
+  forecast_MCS = vector(mode="list", length=length(i_vec))
+  names(forecast_MCS) = i_vec
+  for (k in i_vec) {
+    forecast_MCS[[k]] =pred.n.ahead(u, test.period, parms[[2]][[k]][[1]])
   }
-  return (loss[-1,])
+  forecast_MCS = as.data.frame(forecast_MCS)
+  error_grid = (tail(forecast_grid, test.period) - tail(y, test.period))^2
+  error_MCS = tail(apply(forecast_MCS, 2, function(x) x - y), test.period)
+  error_MCS = apply(error_MCS, 1, function(x) mean(x))^2
+  return(list(mean(error_grid), mean(error_MCS)))
+  
 }
+comparison = vector(mode="list", length=2)
+errs = as.data.frame(comparison)
 
-a = loss_values(alpha, phi, sigma2)
-cor(a)
-summary(a)
+  i=1.2
+  for (j in 1:1000) {
+    y = arima.sim(n = 100, list(ar=c(phi)), sd=sqrt(i), mean=alpha)
+    err = as.data.frame(forecast_errors(y, 20, 16, 1:8))
+    names(err)=c("grid", "MCS")
+    errs = rbind(errs, as.data.frame(err))
+    if(j %% 10==0) print(j)
+  }
+t = mean(errs$MCS-errs$grid)/sqrt(var((errs$MCS-errs$grid)))
+errs$diff = errs$MCS < errs$grid
+sum(errs$diff)
 
 mins = apply(a, 1, min)
 mins = data.table("minimal observed losses"= mins)
@@ -124,10 +127,4 @@ fit1 <- fitdistr(mins$`minimal observed losses`, "exponential")
 ks.test(mins$`minimal observed losses`, "pexp", fit1$estimate)
 fit1
 
-
-#######mcs
-number_cores <- detectCores()
-cl <- makeCluster(number_cores)
-MCS <- MCSprocedure(Loss=a,alpha=0.2,B=5000,statistic='Tmax',cl=cl)
-MCS
 
